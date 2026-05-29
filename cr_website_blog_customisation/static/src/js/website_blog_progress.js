7b803a68b8a5e4c4f8fcfa67b84bdaf5861110e9
creyox-sujal <sujal.creyox@gmail.com>
2026-05-29 11:52:47 +0530

    [REMOVED] style for the blog post contents, but keep the custom classes added for content through js.

--- cr_website_blog_customisation/static/src/js/website_blog_progress.js
+++ cr_website_blog_customisation/static/src/js/website_blog_progress.js
@@ -13,24 +13,35 @@ odoo.define('cr_website_blog_customisation.blog_progress_bar', function (require
         start: function () {
             this.$progressWrap = this.$('#cr-blog-progress-circle-wrap');
             this.$content = this.$('#o_wblog_post_content, .o_wblog_post_content_field, #blog_content, [field="post_content"]');
-            
-            // Add custom styling classes to all raw HTML tags inside the blog post content
+
+            // Add custom styling classes to all raw HTML tags inside the blog post content (excluding base layout snippets & custom grids)
             if (this.$content.length) {
-                this.$content.find('p').addClass('cr-blog-paragraph');
-                this.$content.find('h1').addClass('cr-blog-h1');
-                this.$content.find('h2').addClass('cr-blog-h2');
-                this.$content.find('h3').addClass('cr-blog-h3');
-                this.$content.find('h4').addClass('cr-blog-h4');
-                this.$content.find('h5').addClass('cr-blog-h5');
-                this.$content.find('h6').addClass('cr-blog-h6');
-                this.$content.find('strong, b').addClass('cr-blog-bold');
-                this.$content.find('a').addClass('cr-blog-link');
-                this.$content.find('ul').addClass('cr-blog-ul');
-                this.$content.find('ol').addClass('cr-blog-ol');
-                this.$content.find('li').addClass('cr-blog-li');
-                this.$content.find('blockquote').addClass('cr-blog-blockquote');
-                this.$content.find('pre').addClass('cr-blog-pre');
-                this.$content.find('code').addClass('cr-blog-code');
+                // List of Odoo base structural/layout snippets to completely protect and ignore
+                var baseSnippets = [
+                    '.s_cover', '.s_three_columns', '.s_four_columns', '.s_image_text', '.s_text_image',
+                    '.s_features', '.s_comparisons', '.s_call_to_action', '.s_showcase', '.s_banner',
+                    '.s_media_list', '.s_color_blocks', '.s_numbers', '.s_picture', '.s_faq_collapse',
+                    '.s_quotes_carousel', '.s_references', '.s_table_of_content', '.cr_blocks',
+                    '.s_author_card', '.s_blog_toc', '.s_blog_contact_us'
+                ];
+                var excludeFilter = baseSnippets.map(function(s) { return ':not(' + s + ' *)'; }).join('');
+
+                this.$content.find('p').filter(excludeFilter).addClass('cr-blog-paragraph');
+                this.$content.find('h1').filter(excludeFilter).addClass('cr-blog-h1');
+                this.$content.find('h2').filter(excludeFilter).addClass('cr-blog-h2');
+                this.$content.find('h3').filter(excludeFilter).addClass('cr-blog-h3');
+                this.$content.find('h4').filter(excludeFilter).addClass('cr-blog-h4');
+                this.$content.find('h5').filter(excludeFilter).addClass('cr-blog-h5');
+                this.$content.find('h6').filter(excludeFilter).addClass('cr-blog-h6');
+                this.$content.find('strong, b').filter(excludeFilter).addClass('cr-blog-bold');
+                this.$content.find('a:not(.btn):not([class*="btn"]):not(:has(img))').filter(excludeFilter).addClass('cr-blog-link');
+                this.$content.find('ul').filter(excludeFilter).addClass('cr-blog-ul');
+                this.$content.find('ol').filter(excludeFilter).addClass('cr-blog-ol');
+                this.$content.find('li').filter(excludeFilter).addClass('cr-blog-li');
+                this.$content.find('blockquote').filter(excludeFilter).addClass('cr-blog-blockquote');
+                this.$content.find('pre').filter(excludeFilter).addClass('cr-blog-pre');
+                this.$content.find('code').filter(excludeFilter).addClass('cr-blog-code');
+                this.$content.find('img').filter(excludeFilter).addClass('cr-blog-image');
             }
 
             if (this.$progressWrap.length && this.$content.length) {
@@ -41,7 +52,7 @@ odoo.define('cr_website_blog_customisation.blog_progress_bar', function (require
 
                 $(window).on('resize.blog_progress', this._onScrollActive.bind(this));
                 $('#wrapwrap').on('scroll.blog_progress', this._onScrollActive.bind(this));
-                
+
                 this._onScrollActive();
             }
             return this._super.apply(this, arguments);
@@ -53,7 +64,7 @@ odoo.define('cr_website_blog_customisation.blog_progress_bar', function (require
             var contentEl = this.$content[0];
             var rect = contentEl.getBoundingClientRect();
             var viewportHeight = window.innerHeight;
-            var threshold = 75; 
+            var threshold = 75;
 
             if (rect.top > threshold) {
                 this.$progressWrap.removeClass('show');
